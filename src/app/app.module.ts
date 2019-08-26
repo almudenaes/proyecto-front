a28843b523fb8aaaaa3b8e4c736cda7e9c7102b4
Almudena <almudenaespadafor@hotmail.com>
2019-08-26 15:13:56 +0200

    componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { DondeComponent } from './donde/donde.component';
+import { QuienesComponent } from './quienes/quienes.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    DondeComponent,
+    QuienesComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/donde/donde.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donde',
+  templateUrl: './donde.component.html',
+  styleUrls: ['./donde.component.css']
+})
+export class DondeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
