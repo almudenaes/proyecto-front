581643c25647b172f923f805eb05d4766de4a4b4
Almudena <almudenaespadafor@hotmail.com>
2019-09-02 14:54:42 +0200

    Commit chachi para ver si funsiona

---
+++ src/app/user/cabecera-user/cabecera-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecera-user',
+  templateUrl: './cabecera-user.component.html',
+  styleUrls: ['./cabecera-user.component.css']
+})
+export class CabeceraUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
